607873a869c89a5bbca1612c36a217d02c6b6460
Jovica Lelek <jovica.lelek@gmail.com>
2021-05-04 23:13:19 +0200

    Report-Service - Publish message to SNS topic

--- report-service/src/main/java/com/strawberrye/se/backend/reportservice/web/SystemReportController.java
+++ report-service/src/main/java/com/strawberrye/se/backend/reportservice/web/SystemReportController.java
@@ -1,6 +1,7 @@
 package com.strawberrye.se.backend.reportservice.web;
 
 import com.strawberrye.se.backend.reportservice.dto.CoreSystemReport;
+import com.strawberrye.se.backend.reportservice.sns.CoreSystemReportSnsService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,11 +10,24 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 public class SystemReportController {
 
+    private final CoreSystemReportSnsService reportSnsService;
+
+    public SystemReportController(CoreSystemReportSnsService reportSnsService) {
+        this.reportSnsService = reportSnsService;
+    }
+
     @PostMapping("/{deviceId}")
     public CoreSystemReport postReport(@PathVariable int deviceId, @RequestBody CoreSystemReport report) {
         log.info("Device ID: {}", deviceId);
-        log.info("Core System Report:");
+        log.info("Core system report:");
         log.info("{}", report);
+
+        if (reportSnsService.publishCoreSystemReport(report)) {
+            log.info("Core system report published successfully");
+        } else {
+            log.info("Core system report publishing failed");
+        }
+
         return report;
     }
 
