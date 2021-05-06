b07d6b0d68c56d9f684a93b967b3d612f381105d
Jovica Lelek <jovica.lelek@gmail.com>
2021-05-06 23:47:10 +0200

    Report-Save-L - Add new fields and format logs

--- report-save-lambda/src/main/java/com/strawberrye/se/backend/reportsavelambda/ReportSQSEventHandler.java
+++ report-save-lambda/src/main/java/com/strawberrye/se/backend/reportsavelambda/ReportSQSEventHandler.java
@@ -3,17 +3,25 @@ package com.strawberrye.se.backend.reportsavelambda;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.util.List;
 
 public class ReportSQSEventHandler implements RequestHandler<SQSEvent, Void> {
+
+    private final ReportsTableClient reportsTableClient = new ReportsTableClient();
+    private static final Gson gson = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
 
     @Override
     public Void handleRequest(SQSEvent sqsEvent, Context context) {
         List<SQSEvent.SQSMessage> records = sqsEvent.getRecords();
         for (SQSEvent.SQSMessage message :
                 records) {
-            context.getLogger().log(message.getMessageId() + ": " + message.getBody());
+            context.getLogger().log("Message body:");
+            context.getLogger().log(message.getBody());
+            context.getLogger().log("Message attributes:");
+            context.getLogger().log(message.getAttributes().toString());
         }
         return null;
     }
