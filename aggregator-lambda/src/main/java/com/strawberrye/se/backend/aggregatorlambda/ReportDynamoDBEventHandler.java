1a48bdad8a3ab9f789e59a8f7da47122603a6a4e
Jovica Lelek <jovica.lelek@gmail.com>
2021-05-09 17:31:23 +0200

    Aggregator-L - Log partition key

--- aggregator-lambda/src/main/java/com/strawberrye/se/backend/aggregatorlambda/ReportDynamoDBEventHandler.java
+++ aggregator-lambda/src/main/java/com/strawberrye/se/backend/aggregatorlambda/ReportDynamoDBEventHandler.java
@@ -4,10 +4,17 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.DynamodbEvent;
+import com.amazonaws.services.lambda.runtime.events.models.dynamodb.AttributeValue;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.strawberrye.se.backend.aggregatorlambda.dto.CoreSystemReport;
 
 import java.util.List;
+import java.util.Map;
 
 public class ReportDynamoDBEventHandler implements RequestHandler<DynamodbEvent, Void> {
+
+    private static final Gson gson = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
 
     @Override
     public Void handleRequest(DynamodbEvent dynamodbEvent, Context context) {
@@ -18,6 +25,11 @@ public class ReportDynamoDBEventHandler implements RequestHandler<DynamodbEvent,
                 records) {
             logger.log("Record:\n" + record);
             logger.log("Record body:\n" + record.getDynamodb());
+            logger.log("Keys:\n" + record.getDynamodb().getKeys());
+
+            Map<String, AttributeValue> keys = record.getDynamodb().getKeys();
+
+            logger.log("DeviceId: " + keys.get("PK").getS());
         }
 
         return null;
