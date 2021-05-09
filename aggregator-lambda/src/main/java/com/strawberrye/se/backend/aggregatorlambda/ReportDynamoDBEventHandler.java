e384c2f56eb8fad57a4e2673d55c7700fd8c0e80
Jovica Lelek <jovica.lelek@gmail.com>
2021-05-09 21:36:28 +0200

    Aggregator-L - Remove keys logging and add new image logging

--- aggregator-lambda/src/main/java/com/strawberrye/se/backend/aggregatorlambda/ReportDynamoDBEventHandler.java
+++ aggregator-lambda/src/main/java/com/strawberrye/se/backend/aggregatorlambda/ReportDynamoDBEventHandler.java
@@ -4,17 +4,10 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.DynamodbEvent;
-import com.amazonaws.services.lambda.runtime.events.models.dynamodb.AttributeValue;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.strawberrye.se.backend.aggregatorlambda.dto.CoreSystemReport;
 
 import java.util.List;
-import java.util.Map;
 
 public class ReportDynamoDBEventHandler implements RequestHandler<DynamodbEvent, Void> {
-
-    private static final Gson gson = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
 
     @Override
     public Void handleRequest(DynamodbEvent dynamodbEvent, Context context) {
@@ -25,13 +18,8 @@ public class ReportDynamoDBEventHandler implements RequestHandler<DynamodbEvent,
                 records) {
             logger.log("Record:\n" + record);
             logger.log("Record body:\n" + record.getDynamodb());
-            logger.log("Keys:\n" + record.getDynamodb().getKeys());
-
-            Map<String, AttributeValue> keys = record.getDynamodb().getKeys();
-
-            logger.log("DeviceId: " + keys.get("PK").getS());
+            logger.log("New image:\n" + record.getDynamodb().getNewImage());
         }
-
         return null;
     }
 
