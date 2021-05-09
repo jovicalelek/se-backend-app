d843990992b5ac17d7b762c30aafcd677053595f
Jovica Lelek <jovica.lelek@gmail.com>
2021-05-09 22:27:26 +0200

    Aggregator-L - Log total number of charger users

--- aggregator-lambda/src/main/java/com/strawberrye/se/backend/aggregatorlambda/ReportDynamoDBEventHandler.java
+++ aggregator-lambda/src/main/java/com/strawberrye/se/backend/aggregatorlambda/ReportDynamoDBEventHandler.java
@@ -4,21 +4,43 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.DynamodbEvent;
+import com.amazonaws.services.lambda.runtime.events.models.dynamodb.AttributeValue;
+import com.google.gson.JsonSyntaxException;
 
 import java.util.List;
+import java.util.Map;
 
 public class ReportDynamoDBEventHandler implements RequestHandler<DynamodbEvent, Void> {
 
     @Override
     public Void handleRequest(DynamodbEvent dynamodbEvent, Context context) {
-        LambdaLogger logger = context.getLogger();
-        List<DynamodbEvent.DynamodbStreamRecord> records = dynamodbEvent.getRecords();
+        try {
+            LambdaLogger logger = context.getLogger();
+            List<DynamodbEvent.DynamodbStreamRecord> records = dynamodbEvent.getRecords();
 
-        for (DynamodbEvent.DynamodbStreamRecord record :
-                records) {
-            logger.log("Record:\n" + record);
-            logger.log("Record body:\n" + record.getDynamodb());
-            logger.log("New image:\n" + record.getDynamodb().getNewImage());
+            for (DynamodbEvent.DynamodbStreamRecord record :
+                    records) {
+                logger.log("Record:\n" + record);
+                logger.log("Record body:\n" + record.getDynamodb());
+                logger.log("New image:\n" + record.getDynamodb().getNewImage());
+
+                Map<String, AttributeValue> item = record.getDynamodb().getNewImage();
+
+                item.get("DataGroups").getL().get(0).getM().get("ChargerReports").getL().get(0).getM().get("NumberOfUsers").getN();
+
+                int totNumOfUsers = 0;
+                for (AttributeValue dataGroup :
+                        item.get("DataGroups").getL()) {
+                    for (AttributeValue chargerReport:
+                         dataGroup.getM().get("ChargerReports").getL()) {
+                        totNumOfUsers += Integer.parseInt(chargerReport.getM().get("NumberOfUsers").getN());
+                    }
+                }
+
+                logger.log("Total number of users: " + totNumOfUsers);
+            }
+        } catch (JsonSyntaxException e) {
+            e.printStackTrace();
         }
         return null;
     }
